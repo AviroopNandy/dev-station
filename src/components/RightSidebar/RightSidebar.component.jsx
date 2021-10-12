d14a35db6774399a283efce85806b8b030af4a54
AviroopNandy <aviroopnandy2001@gmail.com>
2021-10-13 01:46:47 +0530

    created: people to follow

--- src/components/RightSidebar/RightSidebar.component.jsx
+++ src/components/RightSidebar/RightSidebar.component.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Button, CircularProgress } from "@material-ui/core";
 
 import "./RightSidebar.style.css";
 
@@ -26,20 +27,61 @@ const RightSidebar = () => {
 
     return (
         <div className="rightSidebar">
-            {/* <h2>Right Side Bar</h2> */}
-            <div className="rightSidebar__profile">
-                {user}'s Profile Card here
+            <div className="rightSidebar__card">
+                <h2>People to follow</h2>
                 <br />
                 {userDetails ? (
                     <>
-                        Followers: {userDetails.followers_count}
-                        <br />
-                        Following: {userDetails.following_count}
-                        <br />
-                        Posts: {userDetails.posts_id.length}
+                    <div className="rightSidebar__user">
+                        <div className="rightSidebar__follow">
+                            <p>Aviroop Nandy @aviroop_nandy</p>
+                            <Button variant="contained">Follow</Button>
+                        </div>
+                        <div className="rightSidebar__desc">
+                            <p><b>10</b> followers, <b>8</b> following, <b>5</b> posts</p>
+                        </div>
+                    </div>
+                    <div className="rightSidebar__user">
+                        <div className="rightSidebar__follow">
+                            <p>Aviroop Nandy @aviroop_nandy</p>
+                            <Button variant="contained">Follow</Button>
+                        </div>
+                        <div className="rightSidebar__desc">
+                            <p>10 followers, 8 following, 5 posts</p>
+                        </div>
+                    </div>
+                    <div className="rightSidebar__user">
+                        <div className="rightSidebar__follow">
+                            <p>Aviroop Nandy @aviroop_nandy</p>
+                            <Button variant="contained">Follow</Button>
+                        </div>
+                        <div className="rightSidebar__desc">
+                            <p>10 followers, 8 following, 5 posts</p>
+                        </div>
+                    </div>
+                    <div className="rightSidebar__user">
+                        <div className="rightSidebar__follow">
+                            <p>Aviroop Nandy @aviroop_nandy</p>
+                            <Button variant="contained">Follow</Button>
+                        </div>
+                        <div className="rightSidebar__desc">
+                            <p><b>10</b> followers, <b>8</b> following, <b>5</b> posts</p>
+                        </div>
+                    </div>
+                    <div className="rightSidebar__user">
+                        <div className="rightSidebar__follow">
+                            <p>Aviroop Nandy @aviroop_nandy</p>
+                            <Button variant="contained">Follow</Button>
+                        </div>
+                        <div className="rightSidebar__desc">
+                            <p><b>10</b> followers, <b>8</b> following, <b>5</b> posts</p>
+                        </div>
+                    </div>
                     </>
                 ) : (
-                    null
+                    <div className="loading">
+                        <CircularProgress className="loader" />
+                    </div>
                 )}
             </div>
         </div>
